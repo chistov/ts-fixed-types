49342734d7363e11eff850f4eb20c42be3b4f1b7
Sergey Chistov <s.chistov@globus-ltd.com>
2022-02-14 08:37:11 +0300

    eslint added

--- medium2.ts
+++ medium2.ts
@@ -3,7 +3,7 @@
 // Нужно заменить FIXME на правильный тип
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+// type FIXME = any;
 
 // Hint: infer
 export const getDefaultProps = <T>(
